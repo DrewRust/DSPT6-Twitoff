4268a6c025875702795745de4fbbaf7977317431
bruno-janota <bruno.janota1@gmail.com>
2020-08-12 23:17:41 -0400

    Update app.py to load env variables

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,13 +1,17 @@
+from os import getenv
 from flask import Flask, render_template, request
+from dotenv import load_dotenv
 from .db_model import db, User
 from .twitter import add_user_tweepy, update_all_users
 from .predict import predict_user
+
+load_dotenv()
 
 def create_app():
     '''Create and configure an instance of the Flask application'''
 
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///C:\\Users\\bruno\\Desktop\\DSPT6-Twitoff\\twitoff\\twitoff.sqlite"
+    app.config["SQLALCHEMY_DATABASE_URI"] = getenv('DATABASE_URL')
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
